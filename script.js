df028ebaaad4df3497c67f5a6b5423e6ee92222e
haskcoder <kalinowskik234@gmail.com>
2024-07-20 12:11:41 +0200

    Updated quote text and author to be empty, changed birthplace description, and added script tag to index.html

---
+++ script.js
@@ -0,0 +1,37 @@
+const quoteText = document.querySelector("#quote-text");
+const quoteAuthor = document.querySelector("#quote-author");
+
+const quotes = [
+    {
+        qt:"He is a poet who awakened the nation to fight for freedom.",
+        qa:"Ignacy Jan Paderewski"
+    },
+    {
+        qt:"Mickiewicz is a genius whose creative power knew no bounds. His words still resonate in the hearts of Poles, inspiring successive generations.",
+        qa:"Henryk Sienkiewicz"
+    },
+    {
+        qt:"Adam Mickiewicz is a poet who made the Polish language more beautiful and richer through his works. His poetry is a true national treasure.",
+        qa:"Czesław Miłosz"
+    },
+    {
+        qt:"Mickiewicz is a poet of heart and soul, whose verses carry extraordinary emotional strength. His works move and touch everyone fortunate enough to know them.",
+        qa:"Maria Konopnicka"
+    },
+    {
+        qt:"Adam Mickiewicz is a master of words who could capture both the beauty of nature and the depth of human feelings in his works. His poetry is a true feast for the spirit",
+        qa:"Zygmunt Krasiński"
+    },
+    {
+        qt:"He is a poet who awakened the nation to fight for freedom.",
+        qa:"Ignacy Jan Paderewski"
+    },
+]
+
+function quoteUpdate(){
+    const random = Math.floor(Math.random() * quotes.length);
+    quoteText.textContent = quotes[random].qt;
+    quoteAuthor.textContent = quotes[random].qa;
+}
+
+quoteUpdate();
